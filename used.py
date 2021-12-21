b0d192f757c1a8e02eb5f7790b0b2e384b5a54be
tomisblue96 <46725061+tomisblue96@users.noreply.github.com>
2021-12-21 17:01:18 +0900

    Update used.py
    book_price 값이 정수값이 되도록 수정
    에러를 SEND_ERROR_TO_TELEGRAM 상수로 제어

--- used.py
+++ used.py
@@ -22,6 +22,12 @@ presents = set()
 previous = set()
 
 MAX_PRICE = 24900
+SEND_ERROR_TO_TELEGRAM = 1
+
+
+def number_filter(text):
+    found_nums = filter(str.isdigit, text)
+    return int(''.join(found_nums))
 
 
 def notice_used():
@@ -36,40 +42,42 @@ def notice_used():
         response = session.get(url, headers=header)
         response.raise_for_status()
 
-        # 중고도서 목록 수집
+        # 중고도서 페이지 수집
         soup = BeautifulSoup(response.content, "html.parser")
         items = soup.select("td[width='25%']")
 
         for index, item in enumerate(items):
-            # print(item.select_one("td a")['href'])
+            # 중고도서 정보 파싱
             book_name = item.select_one("a.bo").previous_sibling + item.select_one("a.bo").text
             book_writer = item.select_one("span.gw").text
-            book_price = item.select_one("td span.").text \
-                         + item.select_one("td span.").next_sibling \
+            book_price = number_filter(item.select_one("span.gw").next_sibling.text)
+            book_sale = item.select_one("span.gw").next_sibling.text \
+                         + item.select_one("span.p1_n").previous_sibling \
                          + item.select_one("span.p1_n").text
             book_link = item.select_one("td a")['href']
-            book_link_code = book_link.replace('https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=', '')
+            book_code = book_link.replace('https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=', '')
+            presents.add(book_code)
 
+            # 새로 업데이트된 중고도서일 경우 텔레그렘 메세지 전송
             if not (n == 3 and index > 40):
-                if previous and book_link_code not in previous and book_price >= MAX_PRICE:
-                    info = '\n'.join([book_name, book_writer, book_price, book_link])
+                if previous and book_code not in previous and book_price >= MAX_PRICE:
+                    info = '\n'.join([book_name, book_writer, book_sale, book_link])
                     print(info)
                     bot.sendMessage(chat_id=chat_id, text=info)
 
-            presents.add(book_link_code)
-
-    print(presents)
-    print(previous)
-
 
 def main():
+    bot.sendMessage(chat_id=chat_id, text="Hello!")
     while True:
-        try:
+        if SEND_ERROR_TO_TELEGRAM:
+            try:
+                notice_used()
+            except:
+                error = '[비싼 알라딘 중고]' + traceback.format_exc().splitlines()[-1]
+                print(error)
+                error_bot.sendMessage(chat_id=error_chat_id, text=error)
+        else:
             notice_used()
-        except:
-            error = '[비싼 알라딘 중고]' + traceback.format_exc().splitlines()[-1]
-            print(error)
-            error_bot.sendMessage(chat_id=chat_id, text=error)
         time.sleep(30)
 
 
